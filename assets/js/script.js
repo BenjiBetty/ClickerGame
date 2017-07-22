b16677844a8113e17d5a8b3937d4b5eedc6b575e
benjibetty <mellecbenjamin@gmail.com>
2017-07-22 22:05:22 +0200

    Ajout du multiplicateur payant et score seulement positif

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,23 @@ var score = 0;
 var bouton = document.getElementById("bouton");
 var affichage = document.getElementById("affichage");
 var multiplicateur = 1;
+var multiplier = document.getElementById("multiplier");
+var multiplicateurCost = 50;
 
 
 /* fonction de clique avec ajout de 1*/
 function add() {
-    score = score + 1;
+    score = score + multiplicateur;
     document.getElementById("affichage").innerHTML = score;
+    document.title = score + " km";
 }
 
 /* fonction pour multiplier */
 function augmenterMultiplicateur() {
-
+    if (score >= multiplicateurCost) {
+        multiplicateur = multiplicateur + 1;
+        score = score - multiplicateurCost;
+        document.getElementById("compteur").innerHTML = multiplicateur;
+        document.getElementById("affichage").innerHTML = score;
+    }
 }
