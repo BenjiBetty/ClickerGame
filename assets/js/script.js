bda79428bcd0d0d0751cdeff1ec902d6b90e5d91
benjibetty <mellecbenjamin@gmail.com>
2017-07-23 21:45:38 +0200

    Fin de l'etape 10 et debut de creation de l'autoclicker.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,15 @@ var affichage = document.getElementById("affichage");
 var multiplicateur = 1;
 var multiplier = document.getElementById("multiplier");
 var multiplicateurCost = 50;
+var autoClickerCost = 200;
+var autoClicker = 1;
 
 
 /* fonction de clique avec ajout de 1*/
 function add() {
     score = score + multiplicateur;
     document.getElementById("affichage").innerHTML = score;
-    document.title = score + " km";
+    document.title = "Morty est à " + score + " km";
 }
 
 /* fonction pour multiplier */
@@ -19,7 +21,17 @@ function augmenterMultiplicateur() {
     if (score >= multiplicateurCost) {
         multiplicateur = multiplicateur + 1;
         score = score - multiplicateurCost;
-        document.getElementById("compteur").innerHTML = multiplicateur;
+        multiplicateurCost = multiplicateurCost * 2;
+        document.getElementById("compteur").innerHTML = "Multiplicateur x" + multiplicateur;
         document.getElementById("affichage").innerHTML = score;
+        document.getElementById("prix").innerHTML = "&#8593; contre " + multiplicateurCost + " km";
     }
+}
+
+function autoClick() {
+    if (score >= autoClickerCost) {
+
+        document.getElementById("prixRickClick").innerHTML = "&#8593; contre " + autoClickerCost;
+    }
+
 }
